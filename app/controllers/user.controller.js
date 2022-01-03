d02a26bf88e4e6ae9df532f94609e253cba5252a
aanusemahu <aanusemahu@gmail.com>
2022-01-03 12:07:43 +0700

    fix routes message payoads

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,15 +1,15 @@
 exports.allAccess = (req, res) => {
-    res.status(200).send("Public Content.");
+    res.status(200).send({'Message' : "Public Content."});
   };
   
-  exports.userBoard = (req, res) => {
-    res.status(200).send({'Message' : "User Content."});
-  };
-  
-  exports.adminBoard = (req, res) => {
-    res.status(200).send("Admin Content.");
-  };
-  
-  exports.moderatorBoard = (req, res) => {
-    res.status(200).send("Moderator Content.");
-  };
+exports.userBoard = (req, res) => {
+  res.status(200).send({'Message' : "User Content."});
+};
+
+exports.adminBoard = (req, res) => {
+  res.status(200).send({'Message' : "Admin Content."});
+};
+
+exports.moderatorBoard = (req, res) => {
+  res.status(200).send({'Message' : "Moderator Content."});
+};
